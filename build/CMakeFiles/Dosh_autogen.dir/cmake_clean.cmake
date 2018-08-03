0c3bc580f0e8bc05e295436da89af393ae6ecbe6
3igzag <mzadatert@gmail.com>
2018-08-03 03:09:43 -0700

    The Wallet

---
+++ build/CMakeFiles/Dosh_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Dosh_autogen"
+  "CMakeFiles/Dosh_autogen.dir/AutogenOldSettings.cmake"
+  "cryptonote_autogen"
+  "CMakeFiles/cryptonote_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/Dosh_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Dosh_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/rocksdb.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Dosh_autogen"
+  "CMakeFiles/Dosh_autogen.dir/AutogenOldSettings.cmake"
+  "cryptonote_autogen"
+  "CMakeFiles/cryptonote_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/rocksdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/rocksdb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
